43e5676378b1717373f3a666ccde5e6b40d1a953
Colby Kauk <colby.kauk@gmail.com>
2022-01-20 12:04:30 -0800

    initial commit

---
+++ src/Components/SubContents.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styles from './blockcomponents.module.css';
+
+class SubContents extends Component{
+    render(){
+        return(
+            <div className= {styles.subcontents}></div>
+        )
+    }
+}
+
+export default SubContents;
